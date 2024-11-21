6295d659431f1f83a6aec1f5c21e0a2a2a158f37
Nicolas Salguero <nsalguero@outlook.es>
2024-11-21 16:43:46 -0300

    SIP ventas cambio menor en index

--- routes/usuarios/index.js
+++ routes/usuarios/index.js
@@ -7,13 +7,13 @@ var adminRouter = require('./admin');
 const { SECRETJWT, TOKEN_ADMIN } = process.env;
 
 router.use(function(req, res, next) {
-  const token = req.headers.authorization;
+  const token = req.headers.authorization;  
   if (token !== undefined && token !== null){
     if(token === TOKEN_ADMIN) return(adminRouter(req, res, next));
     try {
       let decoded = jwt.verify(token, SECRETJWT);
       req.user_id = decoded.data.id;
-      
+      console.log(decoded.data.tipo);      
       if(decoded.data.tipo === 2) return(adminRouter(req, res, next));
     } catch (error) { 
       if (error.name === 'JsonWebTokenError')
