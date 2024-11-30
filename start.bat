af132aaec9baea7f21c88e59ac5644080434d7c2
nsalguero <nsalguero@outlook.es>
2024-11-30 12:58:59 -0300

    cambios en archivo .bat

--- start.bat
+++ start.bat
@@ -14,15 +14,23 @@ git diff --quiet origin/build
 if %errorlevel% neq 0 (
     echo Hay cambios en la rama origin/build. Descargando cambios...
     git pull origin build
-    
-    :: Ejecutar npm install
+
+    :: Ejecutar npm install solo si hay cambios
     echo Ejecutando npm install...
     npm install
+
+    :: Mensaje para invitar al usuario a ejecutar nuevamente el script
+    echo.
+    echo Las dependencias han sido actualizadas. 
+    echo Por favor, ejecuta nuevamente este script para continuar.
+    echo.
+    pause
+    exit /b
 ) else (
     echo No hay cambios en la rama origin/build.
 )
 
-:: Ejecutar npm run start
+:: Ejecutar npm run start si no hubo cambios
 echo Ejecutando npm run start...
 start cmd /k "npm run start"
 
