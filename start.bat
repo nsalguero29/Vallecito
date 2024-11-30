5f1ecb4311fa5a480887edf8b0f2663013e1ae0b
nsalguero <nsalguero@outlook.es>
2024-11-30 12:51:08 -0300

    modificacion bat

--- start.bat
+++ start.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal
 
-:: Cambiar al directorio del proyecto (ajusta la ruta según sea necesario)
-:: cd "C:\ruta\de\tu\proyecto"
+:: Cambiar al directorio del script
+cd /d "%~dp0"
 
 :: Verificar si hay cambios en la rama origin/build
 echo Verificando cambios en la rama origin/build...
@@ -14,13 +14,13 @@ git diff --quiet origin/build
 if %errorlevel% neq 0 (
     echo Hay cambios en la rama origin/build. Descargando cambios...
     git pull origin build
+    
+    :: Ejecutar npm install
+    echo Ejecutando npm install...
+    npm install
 ) else (
     echo No hay cambios en la rama origin/build.
 )
-
-:: Ejecutar npm install
-echo Ejecutando npm install...
-npm install
 
 :: Ejecutar npm run start
 echo Ejecutando npm run start...
@@ -29,5 +29,10 @@ start cmd /k "npm run start"
 :: Abrir el navegador con la URL
 echo Abriendo el navegador en http://localhost:3000/
 start http://localhost:3000/
+
+:: Pausar para que la ventana permanezca abierta
+echo.
+echo Presiona cualquier tecla para cerrar...
+pause
 
 endlocal
